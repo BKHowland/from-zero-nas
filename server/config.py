7a438616ed30a32697078acfde60935534914df3
BKHowland <BenjaminKHowland@gmail.com>
2025-04-29 19:55:29 -0700

    Removed Python Code
    Added Golang serves same purpose. Pivoting all python to Go. no longer needed.

--- server/config.py
+++
@@ -1,9 +0,0 @@
-# Server-side configuration
-
-HOST = '0.0.0.0'        # Listen on all network interfaces
-PORT = 5000             # Port to bind the server to
-
-STORAGE_DIR = 'storage' # Folder to store uploaded files
-MAX_CONNECTIONS = 5     # Max queued clients
-BUFFER_SIZE = 4096
-TIMEOUT = 10
