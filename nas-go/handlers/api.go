090562122d082ee86405621f0058ade911bcf5d6
BKHowland <BenjaminKHowland@gmail.com>
2025-04-29 19:47:17 -0700

    client request logging
    Added terminal logging for client requests

--- nas-go/handlers/api.go
+++ nas-go/handlers/api.go
@@ -2,10 +2,12 @@ package handlers
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
 func ApiSubmitHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Received request:", r.Method, r.URL.Path, "-", r.RemoteAddr) // Log the request in terminal
 	if r.Method == "POST" {
 		var data map[string]string
 		json.NewDecoder(r.Body).Decode(&data)
