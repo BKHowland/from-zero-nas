923b1ec6e443356132c07cb2065072151f4f354e
BKHowland <BenjaminKHowland@gmail.com>
2025-05-07 14:28:46 -0700

    added client side button
    added button and handler function for client download interaction

---
+++ static/download.js
@@ -0,0 +1,8 @@
+
+
+function downloadFiles(fullPath) {
+    // sends a request to the server to download the requested file or folder. 
+    console.log("User download request for file: ", fullPath);
+
+}
+
