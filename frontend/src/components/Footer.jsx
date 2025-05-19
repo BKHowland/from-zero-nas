0a80b1205aad32dc43b918aa7fcdb32bfb85d406
BKHowland <BenjaminKHowland@gmail.com>
2025-05-19 14:28:13 -0700

    basic header and footer
    added basic header and footer in place of original app template

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer(){
+
+    return(
+        <footer>
+            <p>&copy; BKHowland <a href="https://github.com/BKHowland">https://github.com/BKHowland</a></p>
+        </footer>
+    );
+}
+
+export default Footer
