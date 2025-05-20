b7e83c22345f1801f851b52c913f159cf6d47da0
BKHowland <BenjaminKHowland@gmail.com>
2025-05-19 17:45:48 -0700

    added file list and forward traversal
    added the required component for showing the file list as well as forward folder traversal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,23 @@
 
+import { useState, useEffect } from 'react'; // required for useState hook 
 import Header from "./components/Header.jsx"
 import Footer from "./components/Footer.jsx"
 // import './app.css'
 import DirectoryHeader from "./components/DirectoryHeader.jsx"
+import FileList from "./components/FileList.jsx"
 
 function App() {
+  const [currentDir, setCurrentDir] = useState('');
+
+  const handleDirectoryClick = (path) => {
+    setCurrentDir(path);
+  };
 
   return (
     <>
       <Header/>
       <DirectoryHeader currentDir={"test"}/>
+      <FileList currentDir={currentDir} onDirectoryClick={handleDirectoryClick} />
       <Footer/>
     </>
   )
--- frontend/src/components/FileList.jsx
+++ frontend/src/components/FileList.jsx
@@ -1,12 +1,29 @@
 
-function FileList({ files, onDirClick }) {
+import { useState, useEffect } from 'react'; // required for useState hook  
+
+function FileList({ currentDir, onDirectoryClick }) {
+    // onDirectoryClick: a callback to handle directory clicks.
+
+    const [files, setFiles] = useState([]);
+
+    // This effect runs every time currentDir changes. this means that onDirectoryClick triggers. 
+    useEffect(() => {
+    fetch(`http://10.0.0.235:8080/reactfilelist?dir=${encodeURIComponent(currentDir)}`) //replace with go server ip/port
+        .then(res => res.json())
+        .then(data => setFiles(data))
+        .catch(err => console.error("Error fetching file list:", err));
+    }, [currentDir]);
+
   return (
     <ul>
       {files.map(file => (
-        <li key={file.path}>
-          {file.name} ({file.size} bytes), IsDirectory: {file.isDirectory.toString()}
-          {file.isDirectory && (
-            <button onClick={() => onDirClick(file.path)}>Enter</button>
+        <li key={file.Path}>
+          {file.IsDirectory ? (
+            <button onClick={() => onDirectoryClick(file.Path)}>
+                {file.Name}
+            </button>
+          ) : (
+            <span> {file.Name} ({file.Size} bytes)</span>
           )}
         </li>
       ))}
