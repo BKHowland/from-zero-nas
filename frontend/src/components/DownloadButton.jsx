f0dbd7394b77920d7cc741096a6dff76b8262290
BKHowland <BenjaminKHowland@gmail.com>
2025-05-20 10:36:09 -0700

    background download support
    Added support to start the download without disrupting user with new tab

--- frontend/src/components/DownloadButton.jsx
+++ frontend/src/components/DownloadButton.jsx
@@ -3,7 +3,16 @@
 function handleDownload(filePath) {
     const downloadUrl = `http://10.0.0.235:8080/download?path=${encodeURIComponent(filePath)}`; //replace with go server ip/port
     // TODO: LATER ITERATIONS MUST GET THIS VALUE FROM A CONFIG FILE TO AVOID MULTIPLE PLACES.
-    window.open(downloadUrl, '_blank');
+    // window.open(downloadUrl, '_blank'); //straightforward but opens new window
+
+    // To support background download without a new page opening:
+    // create hidden link, automatically click, and remove it
+    const link = document.createElement('a');
+    link.href = downloadUrl;
+    link.download = '';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 }
 
 function DownloadButton({ filePath }) {
